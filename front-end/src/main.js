8c01604cb3310e85958515120ddcca0b6c3a507e
Vuong Nguyen <133983525+nguvuong@users.noreply.github.com>
2024-01-19 01:56:41 -0500

    Update main.js

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -10,12 +10,12 @@ import NotFoundPage from './pages/NotFoundPage.vue';
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBQ6ak2u7zhyRRBf2iNF9jGxsPQjKtkGg0",
-  authDomain: "vue-site-301f8.firebaseapp.com",
-  projectId: "vue-site-301f8",
-  storageBucket: "vue-site-301f8.appspot.com",
-  messagingSenderId: "127414253778",
-  appId: "1:127414253778:web:645d4ec8ef61a2e22b550b"
+  apiKey: "AIzaSyCUwVw64TVHiG5JN-c3H1QTHYifnCElyww",
+  authDomain: "vue-site-test-486c6.firebaseapp.com",
+  projectId: "vue-site-test-486c6",
+  storageBucket: "vue-site-test-486c6.appspot.com",
+  messagingSenderId: "378235590581",
+  appId: "1:378235590581:web:c2c06d4934fe2d45cbebfc"
 };
 
 initializeApp(firebaseConfig);
