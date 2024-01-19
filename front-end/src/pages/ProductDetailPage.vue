d731e977a2fb18f3af1a1ff7669fc2cb717e1fef
Vuong Nguyen <133983525+nguvuong@users.noreply.github.com>
2024-01-19 02:06:09 -0500

    Update ProductDetailPage.vue

--- front-end/src/pages/ProductDetailPage.vue
+++ front-end/src/pages/ProductDetailPage.vue
@@ -74,7 +74,7 @@ export default {
       const email = prompt("Please enter your email to sign in:");
       const auth = getAuth();
       const actionCodeSettings = {
-        url: `<THE URL OF THE APPLICATION YOU CREATED IN THE RENDER DASHBOARD>`,
+        url: `https://full-stack-vue-deployment-ijmw.onrender.com`,
         handleCodeInApp: true,
       };
       await sendSignInLinkToEmail(auth, email, actionCodeSettings);
